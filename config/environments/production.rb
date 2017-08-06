4266876351eb8c7e99f1afcd4b5aa5997d9bd743
nikitaselyakh <nickselyakh>
2017-08-06 21:52:09 +0300

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://howtomake.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['https://howtomake.herokuapp.com', 'http://howtomake.herokuapp.com']
+  config.action_cable.url = 'wss://canyou.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://canyou.herokuapp.com', 'http://canyou.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
